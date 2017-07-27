f83f98a30fd56e85559d0bb00509bce8da6f869f
Gear <dev_kr@naver.com>
2017-07-28 02:24:18 +0900

    ParseStruct.hh 파일 문서화 주석 추가

--- include/ParseStruct.hh
+++ include/ParseStruct.hh
@@ -1,5 +1,12 @@
 #pragma once
 
+/**
+ * @file ParseStruct.hh
+ * @date 2017.07.28
+ * @author dev_kr
+ * @brief Dlink 코드 파서의 결과가 생성하는 AST의 노드들을 정의합니다.
+ */
+
 #include <cstdint>
 #include <memory>
 #include <string>
