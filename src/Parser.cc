2943d017bc5d667ea9028f7a021489fab343037d
Gear <dev_kr@naver.com>
2017-07-28 18:12:34 +0900

    코드 인덴팅 탭에서 공백 문자로 변경

--- src/Parser.cc
+++ src/Parser.cc
@@ -2,87 +2,87 @@
 
 namespace Dlink
 {
-	const constexpr char Parser::grammar[] = R"(
-	            # Root Expression
-	            EXPR      <- BIN_ADDSUB
+    const constexpr char Parser::grammar[] = R"(
+                # Root Expression
+                EXPR          <- BIN_ADDSUB
 
-				# Binary Operations
+                # Binary Operations
                 BIN_ADDSUB    <- _ BIN_MULDIV (ADDSUB_OP BIN_MULDIV)*
                 BIN_MULDIV    <- ATOM (MULDIV_OP ATOM)*
 
-                ATOM      <- NUMBER / '(' _ EXPR ')' _
-				
-				# Operators
-                ADDSUB_OP <- < [-+] > _
-                MULDIV_OP <- < [/*] > _
+                ATOM          <- NUMBER / '(' _ EXPR ')' _
 
-				# Literals
-                NUMBER    <- < [0-9]+ > _
+                # Operators
+                ADDSUB_OP     <- < [-+] > _
+                MULDIV_OP     <- < [/*] > _
 
-				# Ignoring Characters
-                ~_        <- [ \t\r\n]*
+                # Literals
+                NUMBER        <- < [0-9]+ > _
+
+                # Ignoring Characters
+                ~_            <- [ \t\r\n]*
     )";
 
-	Parser::Parser()
-	{
-		auto BinaryOP_AST = [](const peg::SemanticValues& v) -> ExpressionPtr
-		{	
-			ExpressionPtr lhs = v[0].get<ExpressionPtr>();
+    Parser::Parser()
+    {
+        auto BinaryOP_AST = [](const peg::SemanticValues& v) -> ExpressionPtr
+        {    
+            ExpressionPtr lhs = v[0].get<ExpressionPtr>();
 
-			for (std::size_t i=1; i < v.size(); i += 2)
-			{
-				auto rhs = v[i+1].get<ExpressionPtr>();
-				auto op = v[i].get<BinaryOperator>();
+            for (std::size_t i=1; i < v.size(); i += 2)
+            {
+                auto rhs = v[i+1].get<ExpressionPtr>();
+                auto op = v[i].get<BinaryOperator>();
 
-				lhs = std::make_shared<BinaryOP>(op, lhs, rhs);
-			}
-			
-			return lhs;
-		};
-		
-		load_grammar(grammar);
-		
-		auto parser = *this;
+                lhs = std::make_shared<BinaryOP>(op, lhs, rhs);
+            }
+            
+            return lhs;
+        };
+        
+        load_grammar(grammar);
+        
+        auto parser = *this;
 
-		parser["BIN_ADDSUB"]      = BinaryOP_AST;
-		parser["BIN_MULDIV"]      = BinaryOP_AST;
-		parser["ADDSUB_OP"]   = [](const peg::SemanticValues& v) -> BinaryOperator
-		{ 
-			char op_ch = static_cast<char>(*v.c_str()); 
-			
-			if(op_ch == '-')
-			{
-				return BinaryOperator::Minus;
-			}
-			else if(op_ch == '+')
-			{
-				return BinaryOperator::Plus;
-			}
+        parser["BIN_ADDSUB"]      = BinaryOP_AST;
+        parser["BIN_MULDIV"]      = BinaryOP_AST;
+        parser["ADDSUB_OP"]       = [](const peg::SemanticValues& v) -> BinaryOperator
+        { 
+            char op_ch = static_cast<char>(*v.c_str()); 
+            
+            if(op_ch == '-')
+            {
+                return BinaryOperator::Minus;
+            }
+            else if(op_ch == '+')
+            {
+                return BinaryOperator::Plus;
+            }
 
-			return BinaryOperator::None;
-		};
+            return BinaryOperator::None;
+        };
 
-		parser["MULDIV_OP"] = [](const peg::SemanticValues& v) -> BinaryOperator
-		{ 
-			char op_ch = static_cast<char>(*v.c_str()); 
-			
-			if(op_ch == '/')
-			{
-				return BinaryOperator::Divide;
-			}
-			else if(op_ch == '*')
-			{
-				return BinaryOperator::Multiply;
-			}
+        parser["MULDIV_OP"]      = [](const peg::SemanticValues& v) -> BinaryOperator
+        { 
+            char op_ch = static_cast<char>(*v.c_str()); 
+            
+            if(op_ch == '/')
+            {
+                return BinaryOperator::Divide;
+            }
+            else if(op_ch == '*')
+            {
+                return BinaryOperator::Multiply;
+            }
 
-			return BinaryOperator::None;
-		};
+            return BinaryOperator::None;
+        };
 
-		parser["NUMBER"]    = [](const peg::SemanticValues& v) -> ExpressionPtr
-		{ 
-			int32_t data = std::atoi(v.c_str()); 
-			ExpressionPtr node = std::make_shared<Integer32>(data);
-			return node;
-		};
-	}
+        parser["NUMBER"]        = [](const peg::SemanticValues& v) -> ExpressionPtr
+        { 
+            int32_t data = std::atoi(v.c_str()); 
+            ExpressionPtr node = std::make_shared<Integer32>(data);
+            return node;
+        };
+    }
 }
