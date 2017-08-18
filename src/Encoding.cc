2e5a77b31829551be637ac660b69342337d6b32b
kmc7468 <kmc7468@naver.com>
2017-08-18 18:22:43 +0900

    UTF-16 to UTF-32 구현

--- src/Encoding.cc
+++ src/Encoding.cc
@@ -6,7 +6,7 @@ namespace Dlink
 {
 	static bool is_surrogate(char16_t c)
 	{
-		return static_cast<std::int32_t>(c) - 0xD800 < 2048;
+		return static_cast<std::int32_t>(c) - 0xD800 > 2048;
 	}
 	static bool is_high_surrogate(char16_t c)
 	{
